b92c3408afe0fa25091dad17f9a8154cb7991f2a
jegedenifemi <jegedenifemi24@gmail.com>
2022-07-11 11:54:29 +0100

    initial commit

---
+++ myScript.js
@@ -0,0 +1,20 @@
+function webDsgnBtn() {
+  document.getElementById("TblRowhead1").innerHTML = "Website Redesign";
+  document.getElementById("TblRowhead2").innerHTML = "Website Renovation";
+  document.getElementById("TblRowhead3").innerHTML = "Marketing Plans";
+  document.getElementById("TblRowhead4").innerHTML = "All-New Website";
+}
+
+function graphicsBtn() {
+  document.getElementById("TblRowhead1").innerHTML = "Graphics Redesign";
+  document.getElementById("TblRowhead2").innerHTML = "Digital Graphics";
+  document.getElementById("TblRowhead3").innerHTML = "New Artworks";
+  document.getElementById("TblRowhead4").innerHTML = "Complex Art";
+}
+
+function webCodingBtn() {
+  document.getElementById("TblRowhead1").innerHTML = "Backend Coding";
+  document.getElementById("TblRowhead2").innerHTML = "New Web App";
+  document.getElementById("TblRowhead3").innerHTML = "Frontend Interactions";
+  document.getElementById("TblRowhead4").innerHTML = "Video Creations";
+}
